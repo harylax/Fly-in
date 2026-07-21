2c9664823a741eb661a750a94ce6d74c1fe05b62
harylax <haryandr23@gmail.com>
2026-07-21 14:55:25 +0300

    update

--- simulation.py
+++ simulation.py
@@ -1,4 +1,4 @@
-from map_model import Map, Hub, Connection, Drone
+from map_model import Map, Hub, Connection, Drone, Zone, Color
 
 
 class Simulation:
@@ -64,16 +64,54 @@ class Simulation:
 
             self.drones_moves.append(self.snapshot())
 
+    def is_dead_end(self, hub: Hub) -> bool:
+        return hub.zone == Zone.blocked or hub.color == Color.red
+
+    def dead_end_solve(self) -> None:
+        turn: int = 0
+
+        while len(self.map.end_hub.current_drones) != self.map.nb_drones:
+            for hub in reversed(self.map.hubs):
+                if hub == self.map.end_hub:
+                    continue
+                if not hub.current_drones:
+                    continue
+
+                for link in self.map.connections:
+                    if link.origin == hub and link.destination:
+
+                        if self.is_dead_end(link.destination):
+                            continue
+
+                        next_hub: Hub = link.destination
+
+                        next_hub_capacity: int = next_hub.max_drones - len(next_hub.current_drones)
+                        nb_to_move: int = min(link.max_link_capacity, next_hub_capacity)
+
+                        for _ in range(nb_to_move):
+                            if not hub.current_drones:
+                                break
+                            drone: Drone = hub.current_drones.pop(0)
+                            next_hub.current_drones.append(drone)
+                            drone.zone = next_hub
+
+            turn += 1
+
+            self.drones_moves.append(self.snapshot())
+
 
 if __name__ == "__main__":
 
     # map = Map('maps/easy/01_linear_path.txt')
-    map = Map('maps/easy/02_simple_fork.txt')
+    # map = Map('maps/easy/02_simple_fork.txt')
+    # map = Map('maps/easy/03_basic_capacity.txt')
+    map = Map('maps/medium/01_dead_end_trap.txt')
 
     simulation = Simulation(map)
 
     # simulation.linear_solve()
-    simulation.simple_fork_solve()
+    # simulation.simple_fork_solve()
+    simulation.dead_end_solve()
 
     result: list[str] = []
     for i, moves in enumerate(simulation.drones_moves):
