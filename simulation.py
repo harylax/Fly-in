74e0fbacc86c3c9a3f24e1176bca2c2f8b2c8bdd
harylax <haryandr23@gmail.com>
2026-07-21 09:37:25 +0300

    update

--- simulation.py
+++ simulation.py
@@ -1,31 +1,77 @@
-from map_model import Map
+from map_model import Map, Drone
+
+# map = Map('maps/easy/01_linear_path.txt')
+# for drone in map.drones:
+#     drone.zone = map.start_hub
+#     map.start_hub.current_drones.append(drone)
+
+# turn: int = 0
+
+# while True:
+#     if len(map.end_hub.current_drones) == map.nb_drones:
+#         break
+
+#     for i in range(len(map.hubs) - 1, 0, -1):
+#         current_hub = map.hubs[i - 1]
+#         next_hub = map.hubs[i]
+#         if current_hub.current_drones:
+#             drone = current_hub.current_drones.pop(0)
+#             next_hub.current_drones.append(drone)
+#             drone.zone = next_hub
+
+#     output: str = ''
+
+#     for drone in map.drones:
+#         if drone.zone:
+#             output += f"D{drone.id}-{drone.zone.name} "
+
+#     turn += 1
+
+#     print(f"turn {turn}: {output}")
 
 
-map = Map('maps/easy/01_linear_path.txt')
-for drone in map.drones:
-    drone.zone = map.start_hub
-    map.start_hub.current_drones.append(drone)
+class Simulation:
+    def __init__(self, map: Map):
+        self.map: Map = map
+        self.drones_moves: list[list[Drone]] = []
+        self.init_drones_pos()
 
-turn: int = 0
+    def init_drones_pos(self) -> None:
+        for drone in self.map.drones:
+            drone.zone = self.map.start_hub
+            self.map.start_hub.current_drones.append(drone)
+        self.drones_moves.append(self.map.drones[:])
 
-while True:
-    if len(map.end_hub.current_drones) == map.nb_drones:
-        break
+    def linear_solve(self) -> None:
+        turn: int = 0
 
-    for i in range(len(map.hubs) - 1, 0, -1):
-        current_hub = map.hubs[i - 1]
-        next_hub = map.hubs[i]
-        if current_hub.current_drones:
-            drone = current_hub.current_drones.pop(0)
-            next_hub.current_drones.append(drone)
-            drone.zone = next_hub
+        while len(self.map.end_hub.current_drones) != self.map.nb_drones:
+            for i in range(len(self.map.hubs) - 1, 0, -1):
+                current_hub = self.map.hubs[i - 1]
+                next_hub = self.map.hubs[i]
+                if current_hub.current_drones:
+                    drone = current_hub.current_drones.pop(0)
+                    next_hub.current_drones.append(drone)
+                    drone.zone = next_hub
 
-    output: str = ''
+            turn += 1
 
-    for drone in map.drones:
-        if drone.zone:
-            output += f"D{drone.id}-{drone.zone.name} "
+            self.drones_moves.append(self.map.drones[:])
 
-    turn += 1
 
-    print(f"turn {turn}: {output}")
+if __name__ == "__main__":
+
+    map = Map('maps/easy/01_linear_path.txt')
+    simulation = Simulation(map)
+    simulation.linear_solve()
+
+    result: list[str] = []
+    for i, moves in enumerate(simulation.drones_moves):
+        line = ''
+        for drone in moves:
+            if drone.zone:
+                line += f"D{drone.id}-{drone.zone.name} "
+        result.append(f"turn {i}: {line}")
+
+    for res in result:
+        print(res)
