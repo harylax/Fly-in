ff324bc5990c3b42c061adb888b3d62bfbb8e11a
harylax <haryandr23@gmail.com>
2026-07-21 11:03:44 +0300

    update

--- simulation.py
+++ simulation.py
@@ -1,46 +1,24 @@
-from map_model import Map, Drone
-
-# map = Map('maps/easy/01_linear_path.txt')
-# for drone in map.drones:
-#     drone.zone = map.start_hub
-#     map.start_hub.current_drones.append(drone)
-
-# turn: int = 0
-
-# while True:
-#     if len(map.end_hub.current_drones) == map.nb_drones:
-#         break
-
-#     for i in range(len(map.hubs) - 1, 0, -1):
-#         current_hub = map.hubs[i - 1]
-#         next_hub = map.hubs[i]
-#         if current_hub.current_drones:
-#             drone = current_hub.current_drones.pop(0)
-#             next_hub.current_drones.append(drone)
-#             drone.zone = next_hub
-
-#     output: str = ''
-
-#     for drone in map.drones:
-#         if drone.zone:
-#             output += f"D{drone.id}-{drone.zone.name} "
-
-#     turn += 1
-
-#     print(f"turn {turn}: {output}")
+from map_model import Map
 
 
 class Simulation:
     def __init__(self, map: Map):
         self.map: Map = map
-        self.drones_moves: list[list[Drone]] = []
+        self.drones_moves: list[list[tuple[int, str]]] = []
         self.init_drones_pos()
+
+    def snapshot(self) -> list[tuple[int, str]]:
+        res: list[tuple[int, str]] = []
+        for drone in self.map.drones:
+            if drone.zone:
+                res.append((drone.id, drone.zone.name))
+        return res
 
     def init_drones_pos(self) -> None:
         for drone in self.map.drones:
             drone.zone = self.map.start_hub
             self.map.start_hub.current_drones.append(drone)
-        self.drones_moves.append(self.map.drones[:])
+        self.drones_moves.append(self.snapshot())
 
     def linear_solve(self) -> None:
         turn: int = 0
@@ -56,7 +34,7 @@ class Simulation:
 
             turn += 1
 
-            self.drones_moves.append(self.map.drones[:])
+            self.drones_moves.append(self.snapshot())
 
 
 if __name__ == "__main__":
@@ -68,9 +46,8 @@ if __name__ == "__main__":
     result: list[str] = []
     for i, moves in enumerate(simulation.drones_moves):
         line = ''
-        for drone in moves:
-            if drone.zone:
-                line += f"D{drone.id}-{drone.zone.name} "
+        for drone_id, hub_name in moves:
+            line += f"D{drone_id}-{hub_name} "
         result.append(f"turn {i}: {line}")
 
     for res in result:
